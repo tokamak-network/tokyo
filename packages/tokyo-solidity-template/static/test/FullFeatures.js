ed0c50bdfafeb60ef96aac47a33c9cccf9e7887b
4000D <dkdkajej@gmail.com>
2018-04-13 16:10:59 +0900

    audit: correct input and test for full-features

--- packages/tokyo-solidity-template/static/test/FullFeatures.js
+++ packages/tokyo-solidity-template/static/test/FullFeatures.js
@@ -97,6 +97,7 @@ contract("AuditFullFeaturesCrowdsale", async ([ owner, other, investor1, investo
 
     const numPeriods = input.sale.stages.length;
     for (let i = 0; i < numPeriods; i++) {
+      /* eslint-disable */
       const {
         start_time,
         end_time,
@@ -105,6 +106,7 @@ contract("AuditFullFeaturesCrowdsale", async ([ owner, other, investor1, investo
         min_purchase_limit = 0,
         kyc = false,
       } = input.sale.stages[ i ];
+      /* eslint-enable */
 
       const [
         pCap,
@@ -136,6 +138,9 @@ contract("AuditFullFeaturesCrowdsale", async ([ owner, other, investor1, investo
 
   describe("After start time (stage 0 started)", async () => {
     const targetTime = input.sale.start_time;
+    const stageIndex = 0;
+    const stageMaxPurchaseLimit = input.sale.stages[ stageIndex ].max_purchase_limit;
+    const stageMinPurchaseLimit = input.sale.stages[ stageIndex ].min_purchase_limit;
 
     it("check conditions", async () => {
       (await kyc.registeredAddress(investor1))
@@ -151,7 +156,23 @@ contract("AuditFullFeaturesCrowdsale", async ([ owner, other, investor1, investo
     });
 
     it("reject buying tokens under min purchase", async () => {
-      const investAmount = minEtherAmount.sub(ether(0.01));
+      const investAmount = minEtherAmount.sub(ether(0.0001));
+
+      await sendTransaction({
+        from: investor1, to: crowdsale.address, value: investAmount, gas,
+      }).should.be.rejectedWith(EVMThrow);
+    });
+
+    it("reject buying tokens under stage min purchase", async () => {
+      const investAmount = stageMinPurchaseLimit.mul(0.99999);
+
+      await sendTransaction({
+        from: investor1, to: crowdsale.address, value: investAmount, gas,
+      }).should.be.rejectedWith(EVMThrow);
+    });
+
+    it("reject buying tokens over stage max purchase", async () => {
+      const investAmount = stageMaxPurchaseLimit.mul(1.00001);
 
       await sendTransaction({
         from: investor1, to: crowdsale.address, value: investAmount, gas,
@@ -168,8 +189,9 @@ contract("AuditFullFeaturesCrowdsale", async ([ owner, other, investor1, investo
 
     it("accept buying tokens for valid account and ether amount", async () => {
       const investor = investor1;
-      const investAmount = ether(10); // 390 ether remains for stage 0
-      const rate = getCurrentRate(input, investAmount); // 200 * 1.2
+      const investAmount = ether(40); // 360 ether remains for stage 0
+      const rate = baseRate.mul(1.2); // 200 * 1.2
+      // const rate = getCurrentRate(input, investAmount); // 200 * 1.2
 
       const tokenAmount = investAmount.mul(rate);
 
@@ -194,8 +216,9 @@ contract("AuditFullFeaturesCrowdsale", async ([ owner, other, investor1, investo
     it("accept buying tokens over stage max cap", async () => {
       const investor = investor1;
       const investAmount = ether(400);
-      const purchaseAmount = ether(390);
-      const rate = getCurrentRate(input, purchaseAmount); // 200 * 1.15
+      const purchaseAmount = ether(360);
+      const rate = baseRate.mul(1.3);
+      // const rate = getCurrentRate(input, purchaseAmount); // 200 * 1.3
 
       const tokenAmount = purchaseAmount.mul(rate);
       const tokenBalance = await token.balanceOf(investor);
@@ -220,7 +243,9 @@ contract("AuditFullFeaturesCrowdsale", async ([ owner, other, investor1, investo
 
     it("reject buying tokens when stage is not on sale", async () => {
       const investor = investor2;
-      const investAmounts = range(6).map(i => ether(0.001).mul(10 ** i)); // 0.01 ether, 0.1 ether, .. 1000 ether
+
+      // 0.01 ether, 0.1 ether, .. 1000 ether
+      const investAmounts = range(6).map(i => ether(0.001).mul(10 ** i));
 
       await advanceBlocks();
       for (const investAmount of investAmounts) {
@@ -404,14 +429,23 @@ contract("AuditFullFeaturesCrowdsale", async ([ owner, other, investor1, investo
 
       lockerAmounts.should.be.bignumber.equal(totalSupply.mul(lockerRatio).div(coeff));
       saleAmounts.should.be.bignumber.equal(totalSupply.mul(saleRatio).div(coeff));
+
+      for (const { token_holder, token_ratio } of tokenDistributions) {
+        if (![ "crowdsale", "locker" ].includes(token_holder)) {
+          const tokenAmounts = await token.balanceOf(token_holder);
+          tokenAmounts.should.be.bignumber
+            .equal(totalSupply.mul(token_ratio).div(coeff));
+        }
+      }
     });
   });
 });
 
 function getInput() {
-  return JSON.parse('{"project_name":"Audit Full Features","token":{"token_type":{"is_minime":true},"token_option":{"burnable":true,"pausable":true},"token_name":"For Audit","token_symbol":"FA","decimals":18},"sale":{"max_cap":"4000000000000000000000","min_cap":"1000000000000000000000","start_time":"2018/05/23 00:00:00","end_time":"2018/05/27 00:00:00","coeff":"1000","rate":{"is_static":false,"base_rate":"200","bonus":{"use_time_bonus":true,"use_amount_bonus":true,"time_bonuses":[{"bonus_time_stage":"2018/05/24 00:00:00","bonus_time_ratio":"100"},{"bonus_time_stage":"2018/05/26 00:00:00","bonus_time_ratio":"50"}],"amount_bonuses":[{"bonus_amount_stage":"100000000000000000000","bonus_amount_ratio":"200"},{"bonus_amount_stage":"10000000000000000000","bonus_amount_ratio":"100"},{"bonus_amount_stage":"1000000000000000000","bonus_amount_ratio":"50"}]}},"distribution":{"token":[{"token_holder":"crowdsale","token_ratio":"800"},{"token_holder":"locker","token_ratio":"100"},{"token_holder":"0x557678cf28594495ef4b08a6447726f931f8d787","token_ratio":"100"}],"ether":[{"ether_holder":"0x557678cf28594495ef4b08a6447726f931f8d787","ether_ratio":"800"},{"ether_holder":"0x557678cf28594495ef4b08a6447726f931f8d788","ether_ratio":"200"}]},"stages":[{"start_time":"2018/05/23 00:00:00","end_time":"2018/05/24 00:00:00","cap_ratio":"100","max_purchase_limit":"50000000000000000","min_purchase_limit":"100000000000000","kyc":true},{"start_time":"2018/05/25 00:00:00","end_time":"2018/05/27 00:00:00","cap_ratio":"0","max_purchase_limit":"50000000000000000","min_purchase_limit":"100000000000000","kyc":true}],"valid_purchase":{"max_purchase_limit":"500000000000000000000","min_purchase_limit":"10000000000000000","block_interval":20},"new_token_owner":"0x557678cf28594495ef4b08a6447726f931f8d787"},"multisig":{"use_multisig":true,"infos":[{"num_required":1,"owners":["0x557678cf28594495ef4b08a6447726f931f8d787","0x557678cf28594495ef4b08a6447726f931f8d788"]},{"num_required":1,"owners":["0x557678cf28594495ef4b08a6447726f931f8d789","0x557678cf28594495ef4b08a6447726f931f8d78a"]}]},"locker":{"use_locker":true,"beneficiaries":[{"address":"0x557678cf28594495ef4b08a6447726f931f8d787","ratio":"200","is_straight":true,"release":[{"release_time":"2018/05/28 00:00:00","release_ratio":"300"},{"release_time":"2018/05/30 00:00:00","release_ratio":"1000"}]},{"address":"0x557678cf28594495ef4b08a6447726f931f8d788","ratio":"800","is_straight":false,"release":[{"release_time":"2018/05/27 00:00:00","release_ratio":"200"},{"release_time":"2018/05/28 00:00:00","release_ratio":"500"},{"release_time":"2018/05/30 00:00:00","release_ratio":"1000"}]}]}}');
+  return JSON.parse('{"project_name":"Audit Full Features","token":{"token_type":{"is_minime":true},"token_option":{"burnable":true,"pausable":true},"token_name":"For Audit","token_symbol":"FA","decimals":18},"sale":{"max_cap":"4000000000000000000000","min_cap":"1000000000000000000000","start_time":"2018/05/23 00:00:00","end_time":"2018/05/27 00:00:00","coeff":"1000","rate":{"is_static":false,"base_rate":"200","bonus":{"use_time_bonus":true,"use_amount_bonus":true,"time_bonuses":[{"bonus_time_stage":"2018/05/24 00:00:00","bonus_time_ratio":"100"},{"bonus_time_stage":"2018/05/26 00:00:00","bonus_time_ratio":"50"}],"amount_bonuses":[{"bonus_amount_stage":"100000000000000000000","bonus_amount_ratio":"200"},{"bonus_amount_stage":"10000000000000000000","bonus_amount_ratio":"100"},{"bonus_amount_stage":"1000000000000000000","bonus_amount_ratio":"50"}]}},"distribution":{"token":[{"token_holder":"crowdsale","token_ratio":"800"},{"token_holder":"locker","token_ratio":"100"},{"token_holder":"0x557678cf28594495ef4b08a6447726f931f8d787","token_ratio":"100"}],"ether":[{"ether_holder":"0x557678cf28594495ef4b08a6447726f931f8d787","ether_ratio":"800"},{"ether_holder":"0x557678cf28594495ef4b08a6447726f931f8d788","ether_ratio":"200"}]},"stages":[{"start_time":"2018/05/23 00:00:00","end_time":"2018/05/24 00:00:00","cap_ratio":"100","max_purchase_limit":"400000000000000000000","min_purchase_limit":"100000000000000","kyc":true},{"start_time":"2018/05/25 00:00:00","end_time":"2018/05/27 00:00:00","cap_ratio":"0","max_purchase_limit":"4000000000000000000000","min_purchase_limit":"100000000000000000","kyc":true}],"valid_purchase":{"max_purchase_limit":"500000000000000000000","min_purchase_limit":"10000000000000000","block_interval":20},"new_token_owner":"0x557678cf28594495ef4b08a6447726f931f8d787"},"multisig":{"use_multisig":true,"infos":[{"num_required":1,"owners":["0x557678cf28594495ef4b08a6447726f931f8d787","0x557678cf28594495ef4b08a6447726f931f8d788"]},{"num_required":1,"owners":["0x557678cf28594495ef4b08a6447726f931f8d789","0x557678cf28594495ef4b08a6447726f931f8d78a"]}]},"locker":{"use_locker":true,"beneficiaries":[{"address":"0x557678cf28594495ef4b08a6447726f931f8d787","ratio":"200","is_straight":true,"release":[{"release_time":"2018/05/28 00:00:00","release_ratio":"300"},{"release_time":"2018/05/30 00:00:00","release_ratio":"1000"}]},{"address":"0x557678cf28594495ef4b08a6447726f931f8d788","ratio":"800","is_straight":false,"release":[{"release_time":"2018/05/27 00:00:00","release_ratio":"200"},{"release_time":"2018/05/28 00:00:00","release_ratio":"500"},{"release_time":"2018/05/30 00:00:00","release_ratio":"1000"}]}]}}');
 }
 
+/* eslint-disable complexity,camelcase */
 function getCurrentRate(input, amount) {
   const now = latestTime().unix();
   const {
@@ -455,6 +489,7 @@ function getCurrentRate(input, amount) {
 
   return rate;
 }
+/* eslint-enable complexity,camelcase */
 
 function getEtherAmount(input) {
   if (input.sale.valid_purchase.min_purchase_limit) {
